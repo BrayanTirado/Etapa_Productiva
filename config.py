60b43b31caa600e8de47ca7e08fd0ac379dc0623
Brayan <stivenvargas1615@gmail.com>
2025-09-17 13:13:16 -0500

    Ya esta todo

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 # config.py
-
 class Config:
     #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost:3306/login'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://brayan:brayac@senaproductiva.xyz:3311/bd_brayan'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://brayan:brayanc@isladigital.xyz:3311/bd_brayan'
     SQLALCHEMY_TRACK_MODIFICATIONS = False  
