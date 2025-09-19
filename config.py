9a77e4fc8c5e6d307441411110e7e4205824395e
Brayan <stivenvargas1615@gmail.com>
2025-09-18 21:23:31 -0500

    Creo que ya funciona el gmail.

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ load_dotenv()
 
 class Config:
     #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost:3306/login'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://brayan:brayanc@isladigital.xyz:3311/bd_brayan'
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.abspath("instance/app.db")}'  # Ruta absoluta
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://brayan:brayanc@isladigital.xyz:3311/bd_brayan'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Configuración de email
